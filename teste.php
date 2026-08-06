a082a85d22f6e3c38105c1a5d335dfaf85b438bc
Aluno.Etec <aula@etec.com>
2026-08-06 08:47:27 -0300

    Instalação do Bootstrap

---
+++ teste.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página Inicial</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+ <div class="card-header bg-primary" style="height:60px align-items:'center'">
+    <span class="text-white"><strong>Pesquisar</strong></span>
+  </div>
+  <div class="card-body">
+    
+    <p class="card-text">Menu de pesquisa.</p>
+    <a href="#" class="btn btn-primary">Pesquisar</a>
+  </div>
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+
+</html> 
